5b6ab86f44282ab2730f1fb90a7a698c253bc467
Mawais-10 <fa23-bcs-061@cuiatk.edu.pk>
2026-02-09 21:16:37 +0500

    mad first lab

---
+++ Lab 1/Activity1.js
@@ -0,0 +1,11 @@
+var bio = {
+    name: 'M.Awais',
+    gender: 'male',
+    age: 21,
+    address: {
+        home: 'h#89, st98',
+        city: 'islamabad',
+        country: 'Pakistan'
+    }
+}
+console.log(bio.address.city)
